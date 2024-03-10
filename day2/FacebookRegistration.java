a685e7e8ecae04829adcea0b379aad8b436d0264
karthiksan1965 <162097248+karthiksan1965@users.noreply.github.com>
2024-03-10 17:01:58 +0530

    Add files via upload
    Week 2 Day1 Home assignments

---
+++ day2/FacebookRegistration.java
@@ -0,0 +1,55 @@
+package week3.day1.selenium;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookRegistration {
+
+	public static void main(String[] args) {
+		// Initialize the ChromeDriver
+		ChromeDriver driver = new ChromeDriver();
+		//Load the URL
+		driver.get("https://en-gb.facebook.com/");
+		//Maximize the browser window
+		driver.manage().window().maximize();
+		//Click on the "Create New Account" button.
+		driver.findElement(By.linkText("Create new account")).click();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//Enter the First name
+		driver.findElement(By.name("firstname")).sendKeys("karthik");
+		//Enter the Surname.
+		driver.findElement(By.name("lastname")).sendKeys("san");
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		//Enter the Mobile number or email address.
+		driver.findElement(By.name("reg_email__")).sendKeys("karthiksan1965@gmail.com");
+		//Enter the New password
+		driver.findElement(By.id("password_step_input")).sendKeys("Annakaushik@07");
+		//Handle all three dropdowns in Date of birth
+		WebElement date= driver.findElement(By.id("day"));
+		Select sec = new Select(date);
+		sec.selectByIndex(6);
+		//Select the Month.
+		WebElement Month= driver.findElement(By.id("month"));
+		Select sec2 = new Select(Month);
+		sec2.selectByIndex(5);
+		//Select the Year.
+		WebElement Year= driver.findElement(By.id("year"));
+		Select sec3 = new Select(Year);
+		sec3.selectByValue("1994");
+		//Select the radio button in Gender
+		WebElement radio_male = driver.findElement(By.xpath("//input[@value='2']"));
+		radio_male.click();
+		
+	}
+}
+
+
+
+
+
---
+++ day2/LeaftapsAutomationcreateaccount.java
@@ -0,0 +1,68 @@
+package week3.day1.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LeaftapsAutomationcreateaccount {
+
+	public static void main(String[] args)  {
+
+		// Initialize the ChromeDriver
+		ChromeDriver driver = new ChromeDriver();
+		//Load the URL
+		driver.get("http://leaftaps.com/opentaps/.");
+		//Maximize the browser window
+		driver.manage().window().maximize();
+		//Enter the user name
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		//Enter the Password
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		//Click Login button
+		driver.findElement(By.className("decorativeSubmit")).click();
+		//Click on the "CRM/SFA" link.
+		driver.findElement(By.partialLinkText("CRM")).click();
+		//Click on the "Accounts" tab
+		driver.findElement(By.linkText("Accounts")).click();
+		//Click on the "Create Account" button.
+		driver.findElement(By.linkText("Create Account")).click();
+		//Enter an account name
+		driver.findElement(By.id("accountName")).sendKeys("Testleaf Test account");
+		//Enter a description
+		driver.findElement(By.name("description")).sendKeys("Selenium Automation Tester");
+		//Select the industry.
+		WebElement  industry= driver.findElement(By.name("industryEnumId"));
+		Select sec = new Select(industry);
+		sec.selectByIndex(3);
+		//Select the Ownership.
+		WebElement  Ownership= driver.findElement(By.name("ownershipEnumId"));
+		Select sec2 = new Select(Ownership);
+		sec2.selectByVisibleText("S-Corporation");
+		//Select the Source.
+		WebElement Source= driver.findElement(By.id("dataSourceId"));
+		Select sec3 = new Select(Source);
+		sec3.selectByValue("LEAD_EMPLOYEE");
+		//Select the Marketing Campaign
+		WebElement MarketingCampaign= driver.findElement(By.id("marketingCampaignId"));
+		Select sec4 = new Select(MarketingCampaign);
+		sec4.selectByIndex(6);
+		// Select the State/Province
+		WebElement State= driver.findElement(By.id("generalStateProvinceGeoId"));
+		Select sec5 = new Select(State);
+		sec5.selectByValue("TX");
+		//Click on the "Create Account" button.
+		driver.findElement(By.className("smallSubmit")).click();
+		//Verify that the account name is displayed correctly
+		String expectedAccountName = "Testleaf Test account";
+		WebElement actualAccountNameElement = driver.findElement(By.xpath("//span[contains(text(),'" + expectedAccountName + "')]"));
+		if (actualAccountNameElement.isDisplayed()) {
+			System.out.println("Account name '" + expectedAccountName + "' is displayed correctly.");
+		} else {
+			System.out.println("Account name verification failed!");
+		}
+		//Close the browser window.
+		driver.close();
+
+	}
+}
