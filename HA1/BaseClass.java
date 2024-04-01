1f7af92ff9413d63bce705ef24873ec8d57eabf2
karthiksan1965 <162097248+karthiksan1965@users.noreply.github.com>
2024-04-01 20:41:33 +0530

    Add files via upload
    Week 5 Home Assignments

---
+++ HA1/BaseClass.java
@@ -0,0 +1,38 @@
+package week6.HA1;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseClass 
+{
+	public RemoteWebDriver driver;
+
+	@BeforeMethod
+	public void preCondition() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("disable-notifications");
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+
+	}
+
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+
+	}
+
+}
